64dd35d20fba6edaade2e38eca14afd8706cee0d
muller <pedromullergl11@gmail.com>
2023-09-04 20:42:31 -0300

    test: implemented tests for create a new match

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -5,7 +5,7 @@ import chaiHttp = require('chai-http');
 import { App } from '../../src/app';
 // import token from '../utils/jwt';
 import Match from '../database/models/matchesModel';
-import matchesTeamsMocks from './mocks/match.mocks';
+import matchesTeamsMocks, { createFalseMatchMock, createMatchBody, createdMatchMock, createdWrongIdMock } from './mocks/match.mocks';
 
 chai.use(chaiHttp);
 
@@ -19,5 +19,26 @@ describe('users Test', function() {
     const { status } = await chai.request(app).get('/matches');
 
     expect(status).to.equal(200);
+  });
+  it('should create a new match', async function() {
+    sinon.stub(Match, 'create').resolves(createdMatchMock as any);
+    const { status, body } = await chai.request(app).post('/matches').send(createMatchBody);
+
+    expect(status).to.equal(201);
+    expect(body).to.be.deep.equal(createdMatchMock);
+  });
+  it('should not create a new match with two equals teams Ids', async function() {
+    sinon.stub(Match, 'create').resolves(createdMatchMock as any);
+    const { status, body } = await chai.request(app).post('/matches').send(createFalseMatchMock);
+
+    expect(status).to.equal(422);
+    expect(body).to.equal({ "message": "It is not possible to create a match with two equal teams" });
+  });
+  it('should not create a new match with a unexistent team Id', async function() {
+    sinon.stub(Match, 'create').resolves(createdMatchMock as any);
+    const { status, body } = await chai.request(app).post('/matches').send(createdWrongIdMock);
+
+    expect(status).to.equal(404);
+    expect(body).to.equal({ "message": "There is no team with such id!" });
   });
 });
--- app/backend/src/tests/mocks/match.mocks.ts
+++ app/backend/src/tests/mocks/match.mocks.ts
@@ -29,4 +29,35 @@ const matchesTeamsMocks = [
   }
 ]
 
+const createdMatchMock = {
+  "id": 1,
+  "homeTeamId": 16,
+  "homeTeamGoals": 2,
+  "awayTeamId": 8,
+  "awayTeamGoals": 2,
+  "inProgress": true
+}
+
+const createMatchBody = {
+  "homeTeamId": 16,
+  "awayTeamId": 8,
+  "homeTeamGoals": 2,
+  "awayTeamGoals": 2
+}
+
+const createFalseMatchMock = {
+  "homeTeamId": 8,
+  "awayTeamId": 8,
+  "homeTeamGoals": 2,
+  "awayTeamGoals": 2
+}
+
+const createdWrongIdMock = {
+  "homeTeamId": 999,
+  "awayTeamId": 8,
+  "homeTeamGoals": 2,
+  "awayTeamGoals": 2
+}
+
 export default matchesTeamsMocks;
+export { createdMatchMock, createMatchBody, createFalseMatchMock, createdWrongIdMock };
