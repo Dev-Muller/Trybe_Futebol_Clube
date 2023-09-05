1f5fb48a2b6ab405b33ff4305e41479a78646b06
muller <pedromullergl11@gmail.com>
2023-09-05 10:17:25 -0300

    fix: create new match route

--- app/backend/src/services/matchesService.ts
+++ app/backend/src/services/matchesService.ts
@@ -56,7 +56,7 @@ export default class MatchService {
       awayTeamGoals,
       inProgress: true,
     });
-    // console.log(createNewMatch.dataValues);
-    return { status: 200, data: createNewMatch.dataValues };
+    console.log(createNewMatch);
+    return { status: 201, data: createNewMatch.dataValues };
   }
 }
